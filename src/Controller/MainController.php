1aa35d8cee5129491fe4ed85f1ba06b8b21caeaa
AndreasBoukas <andrew.boukas@gmail.com>
2021-08-26 12:59:33 +0300

    initial heroku commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MainController extends AbstractController
+{
+    #[Route('/api/indices', name: 'indices')]
+    public function showIndices(): Response
+    {
+
+        $projectRoot = $this->getParameter('kernel.project_dir');
+        $strJsonFileContents = file_get_contents($projectRoot.'/resources/indices.json');
+        $indexData = json_decode($strJsonFileContents, true);
+
+        // return $this->render('main/index.html.twig',['indexData' => $indexData]);
+        return new JsonResponse($indexData);
+    }
+
+    #[Route('/api/index/{id}', name: 'index')]
+    public function getindex($id): Response
+    {
+
+        $projectRoot = $this->getParameter('kernel.project_dir');
+        $strJsonFileContents = file_get_contents($projectRoot.'/resources/'.$id.'.json');
+        $indexData = json_decode($strJsonFileContents, true);
+
+        // return $this->render('main/index.html.twig',['indexData' => $indexData]);
+        return new JsonResponse($indexData);
+    }
+}
