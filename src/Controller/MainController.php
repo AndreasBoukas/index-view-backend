bdc8f460ea475ec227efd0aa7d1d1c9a1d40ca25
AndreasBoukas <andrew.boukas@gmail.com>
2021-08-26 13:22:33 +0300

    testing routing

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,24 +10,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/api/indices', name: 'indices')]
+    /**
+     * @Route("/api/indices", name="indices")
+     */
     public function showIndices(): Response
     {
 
         $projectRoot = $this->getParameter('kernel.project_dir');
-        $strJsonFileContents = file_get_contents($projectRoot.'/resources/indices.json');
+        $strJsonFileContents = file_get_contents($projectRoot . '/resources/indices.json');
         $indexData = json_decode($strJsonFileContents, true);
 
         // return $this->render('main/index.html.twig',['indexData' => $indexData]);
         return new JsonResponse($indexData);
     }
 
+    /**
+     * @Route("/api/index/{id}", name="index")
+     */
     #[Route('/api/index/{id}', name: 'index')]
     public function getindex($id): Response
     {
 
         $projectRoot = $this->getParameter('kernel.project_dir');
-        $strJsonFileContents = file_get_contents($projectRoot.'/resources/'.$id.'.json');
+        $strJsonFileContents = file_get_contents($projectRoot . '/resources/' . $id . '.json');
         $indexData = json_decode($strJsonFileContents, true);
 
         // return $this->render('main/index.html.twig',['indexData' => $indexData]);
