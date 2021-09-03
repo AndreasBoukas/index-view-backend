736dbe9be3956f6ee1939203f177815b6b59d3ac
AndreasBoukas <andrew.boukas@gmail.com>
2021-09-03 14:18:44 +0300

    added user already exist error

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class SecurityController extends AbstractController
 {
 
-    public function register(Request $request, UserPasswordHasherInterface $encoder)
+    public function register(Request $request,UserRepository $userRepository, UserPasswordHasherInterface $encoder)
     {
         //registers a user in the database and encodes the password.
         $em = $this->getDoctrine()->getManager();
@@ -21,8 +22,12 @@ class SecurityController extends AbstractController
         $username = $request->request->get('_username');
         $password = $request->request->get('_password');
         if ($username === "" || $password === "") {
-            return new JsonResponse(sprintf('Invalid inputs passed , please check your data. '), 422);
+            return new JsonResponse('Invalid inputs passed , please check your data.', 422);
         }
+        if ($userRepository->findBy(['username' =>  $username])) {
+            return new JsonResponse('The user already exists', 422);
+        }
+
         $user = new User();
         $user->setUsername($username);
         $user->setPassword($encoder->hashPassword($user, $password));
@@ -33,6 +38,6 @@ class SecurityController extends AbstractController
         return new JsonResponse(sprintf('User %s successfully created', $user->getUserIdentifier()));
     }
 
-    //The user log in is handled by the jwt auth package. Check securiy.yaml and lexik_jwt_authentication.yaml and routes.yaml
+    //The user log in is handled by the jwt auth package. Check security.yaml and lexik_jwt_authentication.yaml and routes.yaml
     }
 
